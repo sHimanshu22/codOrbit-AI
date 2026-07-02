84b47d7b3ffb07dbd8d78591a17b45453202dc54
sHimanshu22 <himanshusonawane022@gmail.com>
2026-07-03 00:09:44 +0530

    Production deployment

--- server/src/app.js
+++ server/src/app.js
@@ -21,11 +21,22 @@ const googleAuthRoutes = require("./routes/googleAuthRoutes");
 dotenv.config();
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
+app.use(express.json({ limit: "10mb" }));
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("CodOrbit AI Backend Running");
+  res.status(200).json({
+    success: true,
+    message: "CodOrbit API Running",
+    version: "1.0.0",
+  });
 });
 
 app.use("/api/auth", authRoutes);
@@ -47,11 +58,6 @@ app.use("/api/resume", resumeRoutes);
 
 app.use("/api/google", googleAuthRoutes);
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
-);
+
 
 module.exports = app;
